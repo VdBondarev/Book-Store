d2424fcb6659c42cfba52959128ed6965efe3cd7
Vlad Bondarev <bondarev.21@icloud.com>
2024-05-10 15:02:55 +0300

    tested delete() and getAll() endpoints in UserController

--- src/test/java/book/store/controller/UsersControllerTest.java
+++ src/test/java/book/store/controller/UsersControllerTest.java
@@ -4,24 +4,35 @@ import static book.store.holder.LinksHolder.DELETE_ALL_USERS_FILE_PATH;
 import static book.store.holder.LinksHolder.INSERT_USER_FILE_PATH;
 import static com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.HttpHeaders.AUTHORIZATION;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import book.store.dto.user.UserAdminResponseDto;
 import book.store.dto.user.UserResponseDto;
 import book.store.dto.user.UserUpdateRequestDto;
+import book.store.model.User;
 import book.store.security.JwtUtil;
+import book.store.telegram.strategy.notification.AdminNotificationStrategy;
+import book.store.telegram.strategy.notification.user.UserDeletingNotificationService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -31,15 +42,24 @@ import org.springframework.web.context.WebApplicationContext;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UsersControllerTest {
     protected static MockMvc mockMvc;
+    private static final String PASSWORD = "1234567890";
     private static final String BEARER = "Bearer";
     private static final String EMPTY = " ";
+    private static final String EMAIL = "user@example.com";
     @Autowired
     private ObjectMapper objectMapper;
     @Autowired
     private JwtUtil jwtUtil;
     @Autowired
     private AuthenticationManager authenticationManager;
-    private final String email = "user@example.com";
+    private String jwt;
+    @MockBean
+    private AdminNotificationStrategy<User> notificationStrategy;
+
+    @BeforeEach
+    void setUp() {
+        jwt = jwtUtil.generateToken(EMAIL);
+    }
 
     @BeforeAll
     static void beforeAll(@Autowired WebApplicationContext applicationContext) {
@@ -69,11 +89,9 @@ class UsersControllerTest {
     public void getMyInfo_ValidInput_Success() throws Exception {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
-                        email, "1234567890"
+                        EMAIL, PASSWORD
                 )
         );
-
-        String jwt = jwtUtil.generateToken(email);
 
         MvcResult result = mockMvc.perform(get("/users/mine")
                         .principal(authentication)
@@ -111,11 +129,9 @@ class UsersControllerTest {
     public void updateMyInfo_ValidInput_Success() throws Exception {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
-                        email, "1234567890"
+                        EMAIL, PASSWORD
                 )
         );
-
-        String jwt = jwtUtil.generateToken(email);
 
         UserUpdateRequestDto requestDto = new UserUpdateRequestDto(
                 "New",
@@ -169,17 +185,15 @@ class UsersControllerTest {
     public void updateMyInfo_AlreadyRegisteredEmail_Failure() throws Exception {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
-                        email, "1234567890"
+                        EMAIL, PASSWORD
                 )
         );
-
-        String jwt = jwtUtil.generateToken(email);
 
         UserUpdateRequestDto requestDto = new UserUpdateRequestDto(
                 "New",
                 "New",
                 // expecting that this email is already registered
-                email,
+                EMAIL,
                 "11111111111111"
         );
 
@@ -237,11 +251,9 @@ class UsersControllerTest {
 
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
-                        email, "1234567890"
+                        EMAIL, PASSWORD
                 )
         );
-
-        String jwt = jwtUtil.generateToken(email);
 
         mockMvc.perform(put("/users/mine")
                         .content(content)
@@ -252,4 +264,49 @@ class UsersControllerTest {
                 .andExpect(status().isBadRequest());
     }
 
+    @Sql(
+            scripts = {
+                    DELETE_ALL_USERS_FILE_PATH,
+                    INSERT_USER_FILE_PATH
+            },
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
+    )
+    @Sql(
+            scripts = {
+                    DELETE_ALL_USERS_FILE_PATH
+            },
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD
+    )
+    @DisplayName("""
+            Verify that delete() endpoint works as expected
+            And getAll() as well (after deleting an only user, getAll() should return an empty list)
+            """)
+    @Test
+    @WithMockUser(username = EMAIL, password = PASSWORD, authorities = "ROLE_ADMIN")
+    public void delete_ValidRequest_Success() throws Exception {
+        Long id = 1L;
+
+        when(notificationStrategy.getNotificationService(anyString(), anyString()))
+                .thenReturn(mock(UserDeletingNotificationService.class));
+
+        // deleting a user
+        mockMvc.perform(
+                delete("/users/" + id)
+        )
+                .andExpect(status().isNoContent());
+
+        MvcResult result = mockMvc.perform(
+                        get("/users")
+                )
+                .andExpect(status().isOk())
+                .andReturn();
+
+        UserAdminResponseDto[] users = objectMapper.readValue(
+                result.getResponse().getContentAsString(), UserAdminResponseDto[].class
+        );
+
+        // expecting that there will be no users in db
+        assertEquals(0, users.length);
+    }
 }
+
--- src/test/java/book/store/holder/LinksHolder.java
+++ src/test/java/book/store/holder/LinksHolder.java
@@ -9,4 +9,6 @@ public interface LinksHolder {
     String INSERT_USER_FILE_PATH = "classpath:database/insert-user-to-database.sql";
     String DELETE_ALL_USER_ROLES_FILE_PATH =
             "classpath:database/delete-all-users_roles-from-database.sql";
+    String INSERT_ADMIN_TO_USERS_ROLES_FILE_PATH =
+            "classpath:database/insert-admin-to-user_roles.sql";
 }
